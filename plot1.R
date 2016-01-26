2ee2dd622741c8a3fff5a2372133d9c6a1065157
DimasAdinugroho <dimasadinugroho92@gmail.com>
2016-01-26 23:40:10 +0700

    R Project for Eplatory Graph Analysis Week 1

---
+++ plot1.R
@@ -0,0 +1,13 @@
+#Calculate the frequency of an Active Power between 2007-02-01 and 2007-02-02
+
+file <- "household_power_consumption.txt"
+PowerData <- read.table(file, sep=";" , header=TRUE)
+PowerData$RealDate <- as.Date(PowerData$Date,format='%d/%m/%Y')
+
+newData <- PowerData[which(PowerData$RealDate == "2007-02-01" | PowerData$RealDate == "2007-02-02"),]
+
+png(file = "plo1.png", bg = "transparent", width = 480, height = 480)
+hist(as.integer(newData$Global_active_power)/1000, col = 'red', breaks = 11, xlab="Global Active Power (Kilowatts)", ylab= "Frequency", main ="Global Active Power",yaxt='n')
+axis(side=2, at=seq(0, 1200, by=200))
+
+dev.off()
